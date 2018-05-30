64ee6ea2ea60ee36034f088dc1f05471c1382bad
lihongzhen <lihongzhen@utour.io>
2018-05-30 20:45:03 +0800

    fixed re-approval attack

--- UTOToken.sol
+++ UTOToken.sol
@@ -36,21 +36,13 @@ contract UTOToken {
      * Internal transfer, only can be called by this contract
      */
     function _transfer(address _from, address _to, uint _value) internal {
-        // Prevent transfer to 0x0 address. Use burn() instead
+       // Prevent transfer to 0x0 address. Use burn() instead
         require(_to != 0x0);
-        // Check if the sender has enough
-        require(balanceOf[_from] >= _value);
-        // Check for overflows
-        require(SafeMath.add(balanceOf[_to], _value) >= balanceOf[_to]);
-        // Save this for an assertion in the future
-        uint previousBalances = balanceOf[_from] + balanceOf[_to];
         // Subtract from the sender
-        balanceOf[_from] -= _value;
+        balanceOf[_from] = SafeMath.sub(balanceOf[_from], _value);
         // Add the same to the recipient
-        balanceOf[_to] += _value;
+        balanceOf[_to] = SafeMath.add(balanceOf[_to], _value);
         emit Transfer(_from, _to, _value);
-        // Asserts are used to use static analysis to find bugs in your code. They should never fail
-        assert(balanceOf[_from] + balanceOf[_to] == previousBalances);
     }
 
     /**
@@ -61,8 +53,9 @@ contract UTOToken {
      * @param _to The address of the recipient
      * @param _value the amount to send
      */
-    function transfer(address _to, uint256 _value) public {
+    function transfer(address _to, uint256 _value) public returns (bool success) {
         _transfer(msg.sender, _to, _value);
+        return true;
     }
 
     /**
@@ -74,11 +67,9 @@ contract UTOToken {
      * @param _to The address of the recipient
      * @param _value the amount to send
      */
-    function transferFrom(address _from, address _to, uint256 _value) public returns (bool success) {
-        require(_value <= allowance[_from][msg.sender]);     
+    function transferFrom(address _from, address _to, uint256 _value) public returns (bool success) {   
         // Check allowance
-        // allowance[_from][msg.sender] = SafeMath.sub(allowance[_from][msg.sender], _value);
-        allowance[_from][msg.sender] -= _value;
+        allowance[_from][msg.sender] = SafeMath.sub(allowance[_from][msg.sender], _value);
         _transfer(_from, _to, _value);
         return true;
     }
@@ -92,7 +83,6 @@ contract UTOToken {
      * @param _value the max amount they can spend
      */
     function approve(address _spender, uint256 _value) public returns (bool success) {
-        require(_value <= balanceOf[msg.sender]);
         allowance[msg.sender][_spender] = _value;
         emit Approval(msg.sender, _spender, _value);
         return true;
@@ -115,8 +105,21 @@ contract UTOToken {
         }
     }
 
-    function allowance(address _owner, address _spender) view public returns (uint256 remaining) {
-        return allowance[_owner][_spender];
+    function increaseApproval (address _spender, uint _addedValue) public returns (bool success) {
+        allowance[msg.sender][_spender] = SafeMath.add(allowance[msg.sender][_spender], _addedValue);
+        emit Approval(msg.sender, _spender, allowance[msg.sender][_spender]);
+        return true;
+    } 
+
+    function decreaseApproval (address _spender, uint _subtractedValue) public returns (bool success) {
+        uint oldValue = allowance[msg.sender][_spender];
+        if (_subtractedValue > oldValue) {
+            allowance[msg.sender][_spender] = 0;
+        } else {
+            allowance[msg.sender][_spender] = SafeMath.sub(oldValue, _subtractedValue);
+        }
+        emit Approval(msg.sender, _spender, allowance[msg.sender][_spender]);
+        return true;
     }
 
     /**
@@ -126,12 +129,11 @@ contract UTOToken {
      *
      * @param _value the amount of money to burn
      */
-    function burn(uint256 _value) public returns (bool success) {
-        require(balanceOf[msg.sender] >= _value);   // Check if the sender has enough
-        // balanceOf[msg.sender] = SafeMath.sub(balanceOf[msg.sender], _value);
-        balanceOf[msg.sender] -= _value;            // Subtract from the sender
-        // totalSupply = SafeMath.sub(totalSupply, _value);
-        totalSupply -= _value;                      // Updates totalSupply
+    function burn(uint256 _value) public returns (bool success) { 
+        // Subtract from the sender 
+        balanceOf[msg.sender] = SafeMath.sub(balanceOf[msg.sender], _value); 
+        // Updates totalSupply         
+        totalSupply = SafeMath.sub(totalSupply, _value);                    
         emit Burn(msg.sender, _value);
         return true;
     }
@@ -144,15 +146,13 @@ contract UTOToken {
      * @param _from the address of the sender
      * @param _value the amount of money to burn
      */
-    function burnFrom(address _from, uint256 _value) public returns (bool success) {
-        require(balanceOf[_from] >= _value);                // Check if the targeted balance is enough
-        require(_value <= allowance[_from][msg.sender]);    // Check allowance
-        // balanceOf[_from] = SafeMath.sub(balanceOf[_from], _value);
-        balanceOf[_from] -= _value;                         // Subtract from the targeted balance
-        // allowance[_from][msg.sender] = SafeMath.sub(allowance[_from][msg.sender], _value);
-        allowance[_from][msg.sender] -= _value;             // Subtract from the sender's allowance
-        // totalSupply = SafeMath.sub(totalSupply, _value);
-        totalSupply -= _value;                              // Update totalSupply
+    function burnFrom(address _from, uint256 _value) public returns (bool success) {  
+        // Subtract from the targeted balance
+        balanceOf[_from] = SafeMath.sub(balanceOf[_from], _value);  
+        // Subtract from the sender's allowance
+        allowance[_from][msg.sender] = SafeMath.sub(allowance[_from][msg.sender], _value);
+        // Update totalSupply         
+        totalSupply = SafeMath.sub(totalSupply, _value);                           
         emit Burn(_from, _value);
         return true;
     }
